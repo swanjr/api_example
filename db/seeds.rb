e280e61df12925b951551f91590d425d61a54158
jswan <jswan@gettyimages.com>
2015-07-27 14:14:49 -0500

    Reformated and commented group permissions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,24 +17,54 @@ gcv_reviewer = Group.where(name: 'getty_creative_video_reviewer').first_or_creat
 gev_reviewer = Group.where(name: 'getty_editorial_video_reviewer').first_or_create!
 ges_reviewer = Group.where(name: 'getty_editorial_still_reviewer').first_or_create!
 icv_reviewer = Group.where(name: 'istock_creative_video_reviewer').first_or_create!
+
 puts "Groups created"
 
 create_submission_batch = Permission.where(name: 'create_submission_batch').first_or_create!
 read_submission_batch = Permission.where(name: 'read_submission_batch').first_or_create!
 update_submission_batch = Permission.where(name: 'update_submission_batch').first_or_create!
+
 puts "Permissions created"
 
-super_admin.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
-admin.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
-
-gcv_uploader_nr.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
-gev_uploader_nr.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
-ges_uploader_nr.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
-icv_uploader_nr.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
-
-gcv_uploader_wr.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
-gev_uploader_wr.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
-ges_uploader_wr.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
-icv_uploader_wr.permissions << [create_submission_batch, read_submission_batch, update_submission_batch]
+# Super Admin Permissions
+super_admin.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
+# Admin Permissions
+admin.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
+# Getty Creative Video Uploader No Review Permissions
+gcv_uploader_nr.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
+# Getty Editorial Video Uploader No Review Permissions
+gev_uploader_nr.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
+# Getty Editorial Still Uploader No Review Permissions
+ges_uploader_nr.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
+# iStock Video Uploader No Review Permissions
+icv_uploader_nr.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
+# Getty Creative Video Uploader With Review Permissions
+gcv_uploader_wr.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
+# Getty Editorial Video Uploader With Review Permissions
+gev_uploader_wr.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
+# Getty Editorial Still Uploader With Review Permissions
+ges_uploader_wr.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
+# iStock Video Uploader With Review Permissions
+icv_uploader_wr.permissions << [
+  create_submission_batch, read_submission_batch, update_submission_batch
+]
 
 puts "Permissions assigned to groups"
